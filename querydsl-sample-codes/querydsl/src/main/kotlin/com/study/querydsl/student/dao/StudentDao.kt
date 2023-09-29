8cff154373cd05a2b4da27a9c3b43a2d9bfd3c87
unluckyjung <ybook2012@gmail.com>
2023-09-29 22:06:07 +0900

    feat: Querydsl 에 의존적인 로직 분리

--- querydsl-sample-codes/querydsl/src/main/kotlin/com/study/querydsl/student/dao/StudentDao.kt
+++ querydsl-sample-codes/querydsl/src/main/kotlin/com/study/querydsl/student/dao/StudentDao.kt
@@ -1,11 +1,13 @@
 package com.study.querydsl.student.dao
 
 import com.querydsl.core.BooleanBuilder
+import com.querydsl.core.types.Predicate
 import com.study.querydsl.student.domain.QStudent.student
 import com.study.querydsl.student.domain.Student
 import com.study.querydsl.student.domain.StudentSortType
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport
+import org.springframework.data.querydsl.QSort
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Repository
 
@@ -16,21 +18,57 @@ class StudentDao(
     private val studentRepository: StudentRepository,
 ) : QuerydslRepositorySupport(Student::class.java) {
     fun getStudents(sortType: StudentSortType, size: Long, cursor: Long? = null): List<Student> {
-        val predicate = BooleanBuilder()
-
-        cursor?.let { it ->
-            val cursorStudent =
-                studentRepository.findByIdOrNull(it) ?: throw IllegalArgumentException("잘못된 커서가 들어왔습니다.")
-            predicate.and(
-                sortType.getPredicate(cursorStudent = cursorStudent)
-            )
+        val cursorStudent = cursor?.let { it ->
+            studentRepository.findByIdOrNull(it) ?: throw IllegalArgumentException("잘못된 커서가 들어왔습니다.")
         }
-        
+
+        val studentQuery = StudentQuery(sortType = sortType, cursorStudent = cursorStudent)
+
         val query = from(student)
-            .where(predicate)
+            .where(studentQuery.predicate)
             .limit(size)
 
-        return querydsl!!.applySorting(sortType.qSort, query).fetch()
+        return querydsl!!.applySorting(studentQuery.sort, query).fetch()
     }
 }
 
+internal class StudentQuery(
+    sortType: StudentSortType,
+    cursorStudent: Student? = null,
+) {
+    private val qSortType = StudentQSort.of(sortType = sortType)
+
+    val sort = qSortType.sort
+    val predicate = BooleanBuilder().apply {
+        cursorStudent?.let { this.and(qSortType.getPredicate(it)) }
+    }
+}
+
+internal enum class StudentQSort(val sort: QSort) {
+    AGE_ASC(QSort(student.age.asc(), student.id.asc())) {
+        override fun getPredicate(cursorStudent: Student): Predicate {
+            return student.age.gt(cursorStudent.age).or(
+                (student.age.eq(cursorStudent.age).and(student.id.gt(cursorStudent.id)))
+            )
+        }
+    },
+
+    AGE_DESC(QSort(student.age.desc(), student.id.asc())) {
+        override fun getPredicate(cursorStudent: Student): Predicate {
+            return student.age.lt(cursorStudent.age).or(
+                (student.age.eq(cursorStudent.age).and(student.id.gt(cursorStudent.id)))
+            )
+        }
+    };
+
+    abstract fun getPredicate(cursorStudent: Student): Predicate
+
+    companion object {
+        fun of(sortType: StudentSortType): StudentQSort {
+            return when (sortType) {
+                StudentSortType.AGE_DESC -> AGE_DESC
+                StudentSortType.AGE_ASC -> AGE_ASC
+            }
+        }
+    }
+}
--- querydsl-sample-codes/querydsl/src/main/kotlin/com/study/querydsl/student/domain/StudentSortType.kt
+++ querydsl-sample-codes/querydsl/src/main/kotlin/com/study/querydsl/student/domain/StudentSortType.kt
@@ -1,24 +1,6 @@
 package com.study.querydsl.student.domain
 
-import com.querydsl.core.types.Predicate
-import com.study.querydsl.student.domain.QStudent.student
-import org.springframework.data.querydsl.QSort
-
-enum class StudentSortType(val qSort: QSort) {
-    AGE_ASC(QSort(student.age.asc(), student.id.asc())) {
-        override fun getPredicate(cursorStudent: Student): Predicate {
-            return student.age.gt(cursorStudent.age).or(
-                (student.age.eq(cursorStudent.age).and(student.id.gt(cursorStudent.id)))
-            )
-        }
-    },
-    AGE_DESC(QSort(student.age.desc(), student.id.asc())) {
-        override fun getPredicate(cursorStudent: Student): Predicate {
-            return student.age.lt(cursorStudent.age).or(
-                (student.age.eq(cursorStudent.age).and(student.id.gt(cursorStudent.id)))
-            )
-        }
-    };
-
-    abstract fun getPredicate(cursorStudent: Student): Predicate
+enum class StudentSortType(desc: String) {
+    AGE_ASC(desc = "나이 오름차순"),
+    AGE_DESC(desc = "나이 내림차순"),
 }
